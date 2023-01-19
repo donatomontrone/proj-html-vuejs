7a257d02760aee06afa2c238fd3391622c908792
donatomontrone <donato.montrone@outlook.com>
2023-01-19 13:14:49 +0100

    added about us contents

--- src/components/Main/AboutComoponent.vue
+++ src/components/Main/AboutComoponent.vue
@@ -4,28 +4,68 @@
         <div class="wrapper">
             <div class="d-flex">
                 <div class="about-img">
-                    <p>Inserire img</p>
+                    <img :src="store.getImagePath('img/about.png')" alt="Woman with two dogs">
                 </div>
                 <div class="about-text">
-                    <span>icon</span>
+                    <img :src="store.getImagePath('img/dog-icon.png')" alt="Dog paw icon">
                     <!-- Mini title della section -Colore preso dalla palette -->
-                    <span>about us</span>
-                    <h2>Titolo</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem, minus!</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, minus!</p>
+                    <span class="minititle">About Us</span>
+                    <h2>Welcome DogMilo Pets</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery
+                        of type and scrambled</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery
+                        of type and scrambled</p>
                 </div>
             </div>
         </div>
     </section>
 </template>
 <script>
+import { store } from '../../store.js'
 export default {
-    name: 'AboutComponent'
+    name: 'AboutComponent',
+    data() {
+        return {
+            store,
+        }
+    },
 }
 </script>
 <style lang="scss" scoped>
-div.about-img,
-div.about-text {
-    width: 50%;
+@use '../../styles/partials/variables.scss' as *;
+
+#about-us {
+
+    margin: 4rem 0;
+    background-color: $primary-light-color;
+    background-image: url(../../assets/img/dog-logo.png);
+    background-repeat: no-repeat;
+    background-position: top right;
+    background-size: 190px;
+
+    div.about-img,
+    div.about-text {
+        width: 50%;
+    }
+
+    div.about-img img {
+        width: 365px;
+        margin-left: 1rem;
+    }
+
+    div.about-text {
+        padding: 4rem 0 0 1.5rem;
+
+        img {
+            width: 30px;
+        }
+
+        p {
+            font-size: .85rem;
+        }
+
+    }
 }
 </style>
