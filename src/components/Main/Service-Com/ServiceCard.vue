3ed9e8f899fc6867a6c875bdbaf7d7b39bc29423
donatomontrone <donato.montrone@outlook.com>
2023-01-19 14:46:48 +0100

    added service component contents (cards and imgs)

--- src/components/Main/Service-Com/ServiceCard.vue
+++ src/components/Main/Service-Com/ServiceCard.vue
@@ -1,79 +1,66 @@
+<!--effettuare un v-for al parent -->
 <template>
-    <div class="card-container d-flex justify-content-center">
-        <!-- Dopo cancellare ed effettuare un v-for al parent -->
-        <div class="card-info">
-            <p>icona</p>
-            <p>titolo</p>
-            <p>testo</p>
-            <p>arrow icon</p>
-        </div>
-        <div class="card-info">
-            <p>icona</p>
-            <p>titolo</p>
-            <p>testo</p>
-            <p>arrow icon</p>
-        </div>
-        <div class="card-info">
-            <p>icona</p>
-            <p>titolo</p>
-            <p>testo</p>
-            <p>arrow icon</p>
-        </div>
-        <div class="card-info">
-            <p>icona</p>
-            <p>titolo</p>
-            <p>testo</p>
-            <p>arrow icon</p>
-        </div>
-        <div class="card-info">
-            <p>icona</p>
-            <p>titolo</p>
-            <p>testo</p>
-            <p>arrow icon</p>
+    <div class="card-info d-flex flex-column align-items-center">
+        <img class="path" :src="store.getImagePath('img/' + serviceCardInfo.path)" :alt="serviceCardInfo.title">
+        <h6 class="text-center">{{ serviceCardInfo.title }}</h6>
+        <p class="text-center">{{ serviceCardInfo.text }}</p>
+        <div class="icon d-flex justify-content-center">
+            <img :src="store.getImagePath('img/' + serviceCardInfo.icon)" alt="button">
         </div>
     </div>
 </template>
 <script>
+import { store } from '../../../store.js'
 export default {
     name: 'ServiceCard',
+    props: [
+        'serviceCardInfo'
+    ],
     data() {
         return {
-            cards: [
-                {
-                    icon: 'iconaPath',
-                    title: 'cardtitle',
-                    text: 'cardtext',
-                    arrow: 'arrowicon'
-                },
-                {
-                    icon: 'iconaPath',
-                    title: 'cardtitle',
-                    text: 'cardtext',
-                    arrow: 'arrowicon'
-                },
-                {
-                    icon: 'iconaPath',
-                    title: 'cardtitle',
-                    text: 'cardtext',
-                    arrow: 'arrowicon'
-                },
-                {
-                    icon: 'iconaPath',
-                    title: 'cardtitle',
-                    text: 'cardtext',
-                    arrow: 'arrowicon'
-                },
-                {
-                    icon: 'iconaPath',
-                    title: 'cardtitle',
-                    text: 'cardtext',
-                    arrow: 'arrowicon'
-                },
-            ]
+            store,
         }
     }
 }
 </script>
 <style lang="scss" scoped>
+@use '../../../styles/partials/variables.scss' as *;
 
+div.card-info {
+    background-color: $main-brand-color;
+    width: 140px;
+    border-radius: 100px;
+    padding: 1rem;
+    color: $secondary-light-color;
+
+    &:hover {
+        background-color: $secondary-brand-color;
+    }
+
+    h6 {
+        font-size: .85rem;
+    }
+
+    p {
+        font-size: .7rem;
+        margin-bottom: 1.2rem;
+    }
+
+    img.path {
+        width: 50px;
+        margin-bottom: .8rem;
+    }
+
+    div.icon {
+        background-color: $icon-color;
+        padding: .5rem;
+        border-radius: 100%;
+        width: 40px;
+        height: 40px;
+
+        img {
+            width: 13px;
+        }
+    }
+}
 </style>
--- src/components/Main/Service-Com/ServiceComponent.vue
+++ src/components/Main/Service-Com/ServiceComponent.vue
@@ -1,13 +1,19 @@
 <template>
     <!-- Inserire lo sfondo preso dalla palette ed aggiungere cane a destra e sinistra -->
     <section id="service">
-        <div class="wrapper">
-            <div class="text-center">
-                <!-- Mini title della section - colore preso dalla palette -->
-                <p>Service</p>
-                <h2>titolo</h2>
-                <!-- Effettuare un vfor per ogni card -->
-                <ServiceCard />
+        <div class="bg-left">
+            <div class="bg-right">
+                <div class="wrapper">
+                    <div class="d-flex flex-column align-items-center">
+                        <!-- Mini title della section - colore preso dalla palette -->
+                        <span class="minititle"> Service </span>
+                        <h2 class="text-center">DogMilo Pet Care Services For Your Best Friends.</h2>
+                        <!-- Effettuare un vfor per ogni card -->
+                    </div>
+                    <div class="card-container d-flex justify-content-around">
+                        <ServiceCard v-for="serviceCard in services" :serviceCardInfo="serviceCard" />
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -21,13 +27,70 @@ export default {
     },
     data() {
         return {
-
+            services: [
+                {
+                    path: 'd-1.png',
+                    title: 'Pet Sitting',
+                    text: 'The readable content of a page when looking at its layout.',
+                    icon: 'right.png'
+                },
+                {
+                    path: 'd-2.png',
+                    title: 'Puppy Sitting',
+                    text: 'The readable content of a page when looking at its layout.',
+                    icon: 'right.png'
+                },
+                {
+                    path: 'd-3.png',
+                    title: 'Dog Walking',
+                    text: 'The readable content of a page when looking at its layout.',
+                    icon: 'right.png'
+                },
+                {
+                    path: 'd-4.png',
+                    title: 'Night Care',
+                    text: 'The readable content of a page when looking at its layout.',
+                    icon: 'right.png'
+                },
+                {
+                    path: 'd-5.png',
+                    title: 'Pet Sitting',
+                    text: 'The readable content of a page when looking at its layout.',
+                    icon: 'right.png'
+                },
+            ]
         }
     },
 }
 </script>
 <style lang="scss" scoped>
+@use '../../../styles/partials/variables.scss' as *;
+
 section#service {
-    background-color: grey;
+    padding: 2.7rem 0;
+    background-color: $secondary-light-color;
+
+    div.bg-left {
+        background-image: url(../../../assets/img/dog-left.png);
+        background-repeat: no-repeat;
+        background-position: top left;
+        background-size: 135px;
+    }
+
+    div.bg-right {
+        background-image: url(../../../assets/img/dog-logo.png);
+        background-repeat: no-repeat;
+        background-position: top right;
+        background-size: 190px;
+    }
+
+    h2 {
+        width: 320px;
+        font-size: 1.5rem;
+    }
+
+    span {
+        font-size: .8rem;
+    }
 }
 </style>
