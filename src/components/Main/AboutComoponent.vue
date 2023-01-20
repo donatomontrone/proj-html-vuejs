2ec214fb93dd4742af6e9d94f0722fa2d4329c13
donatomontrone <donato.montrone@outlook.com>
2023-01-20 13:45:02 +0100

    made responsive about us component

--- src/components/Main/AboutComoponent.vue
+++ src/components/Main/AboutComoponent.vue
@@ -1,22 +1,28 @@
 <template>
     <!-- sfondo bianco con cane a destra -->
     <section id="about-us">
-        <div class="wrapper">
-            <div class="d-flex">
-                <div class="about-img">
-                    <img :src="store.getImagePath('img/about.png')" alt="Woman with two dogs">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <div class="about-img text-center">
+                        <img :src="store.getImagePath('img/about.png')" alt="Woman with two dogs" class="img-fluid">
+                    </div>
                 </div>
-                <div class="about-text">
-                    <img :src="store.getImagePath('img/dog-icon.png')" alt="Dog paw icon">
-                    <!-- Mini title della section -Colore preso dalla palette -->
-                    <span class="minititle">About Us</span>
-                    <h2>Welcome DogMilo Pets</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery
-                        of type and scrambled</p>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery
-                        of type and scrambled</p>
+                <div class="col-12 col-md-6">
+                    <div class="about-text pt-lg-5">
+                        <div class="section-title pt-5">
+                            <img :src="store.getImagePath('img/dog-icon.png')" alt="Dog paw icon">
+                            <!-- Mini title della section -Colore preso dalla palette -->
+                            <span class="minititle">About Us</span>
+                        </div>
+                        <h2>Welcome DogMilo Pets</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            gallery of type and scrambled</p>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            gallery of type and scrambled</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -37,35 +43,24 @@ export default {
 @use '../../styles/partials/variables.scss' as *;
 
 #about-us {
-
-    margin: 4rem 0;
+    margin: 6rem 0;
     background-color: $primary-light-color;
     background-image: url(../../assets/img/dog-logo.png);
     background-repeat: no-repeat;
     background-position: top right;
     background-size: 190px;
 
-    div.about-img,
-    div.about-text {
-        width: 50%;
-    }
-
     div.about-img img {
-        width: 365px;
-        margin-left: 1rem;
+        width: 80%;
     }
 
     div.about-text {
-        padding: 4rem 0 0 1.5rem;
-
-        img {
-            width: 30px;
-        }
+        padding-left: 1rem;
 
         p {
-            font-size: .85rem;
+            font-size: 1.15rem;
+            padding-right: 4rem;
         }
-
     }
 }
 </style>
